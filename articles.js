ca89e0b72ce5d8f3bc69e422d7325d5d413441e2
Austin Smith <46660535+asmith747@users.noreply.github.com>
2020-04-28 14:44:47 -0500

    update

--- articles.js
+++ articles.js
@@ -45,76 +45,34 @@ router.get('/api/articles/:articleId', async (req, res) => {
 });
 
 router.get('/api/search/:input/:category/:filter', async (req, res) => {
-  console.log(`${req.params.input} ${req.params.category} ${req.params.filter}`);
-  console.log(`SELECT *
-    FROM articles
-    LEFT OUTER JOIN profiles ON articles.author = profiles.ID
-    LEFT OUTER JOIN (SELECT AVG(ranking) AS rank, author FROM reviews GROUP BY author) AS r ON articles.author = r.author
-    WHERE
-      ((profiles.name LIKE '%${req.params.input}%' AND "${req.params.filter}" != "article") OR (articles.title LIKE '%${req.params.input}%' AND "${req.params.filter}" != "author"))
-        AND
-      (IF("${req.params.category}" != "", (articles.category = "${req.params.category}"), 1=1));`);
 
-      con.getConnection(res, (response) => {
-        if (response.message == 'fail') return;
-        response.conn.query(`SELECT * from articles`, function(err, results, fields) {
-          console.log(results);
-          res.send(results);
-        });
-      });
+  let holder = ''
+  if(req.params.filter == "DESC" || req.params.filter == "ASC")
+    holder = `ORDER BY datePosted ${req.params.filter}`
+  else if(req.params.filter == "rating")
+    holder = `ORDER BY ${req.params.filter} DESC`
+
+  let input = req.params.input
+  if(req.params.input == 0)
+    input = '';
+
+  con.getConnection(res, (response) => {
+
+    if (response.message == 'fail') return;
+    response.conn.query(`SELECT articles.*, (SELECT name FROM profiles WHERE profiles.ID = articles.author) AS authorName, r.rank AS rating
+      FROM articles
+      LEFT OUTER JOIN profiles ON articles.author = profiles.ID
+      LEFT OUTER JOIN (SELECT AVG(ranking) AS rank, author FROM reviews GROUP BY author) AS r ON articles.author = r.author
+      WHERE
+        ((profiles.name LIKE '%${req.params.input}%' AND "${req.params.filter}" != "article")
+          OR (articles.title LIKE '%${req.params.input}%' AND "${req.params.filter}" != "name"))
+          AND (IF("${req.params.category}" != "0", (articles.category = "${req.params.category}"), 1=1))
+      ${holder};`, function(err, results, fields) {
+      res.send(results);
     });
-//   con.getConnection(res, (response) => {
-//     if (response.message == 'fail') return;
-//     response.conn.query(`SELECT articles.*
-//       FROM articles
-//       LEFT OUTER JOIN profiles ON articles.author = profiles.ID
-//       LEFT OUTER JOIN (SELECT AVG(ranking) AS rank, author FROM reviews GROUP BY author) AS r ON articles.author = r.author
-//       WHERE
-//         ((profiles.name LIKE '%${req.params.input}%' AND "${req.params.filter}" != "article") OR (articles.title LIKE '%${req.params.input}%' AND "${req.params.filter}" != "name"))
-//           AND
-//         (IF("${req.params.category}" != "0", (articles.category = "${req.params.category}"), 1=1));`, function(err, results, fields) {
-//       console.log(results);
-//       res.send(results);
-//     });
-//   });
-// });
+  });
+});
 
-// router.get('/api/search/:input/:category/:filter', async (req, res) => {
-//   console.log(`${req.params.input} ${req.params.category} ${req.params.filter}`);
-//   console.log(`SELECT articles.*,
-//   CASE WHEN "${req.params.filter}" = "date" THEN articles.datePosted
-//     WHEN "${req.params.filter}" = "rating" THEN r.rank
-//     ELSE articles.title
-//     END AS sortCol
-//     FROM articles
-//     LEFT OUTER JOIN profiles ON articles.author = profiles.ID
-//     LEFT OUTER JOIN (SELECT AVG(ranking) AS rank, author FROM reviews GROUP BY author) AS r ON articles.author = r.author
-//     WHERE
-//       ((profiles.name LIKE '%${req.params.input}%' AND "${req.params.filter}" != "article") OR (articles.title LIKE '%${req.params.input}%' AND "${req.params.filter}" != "author"))
-//         AND
-//       (IF("${req.params.category}" != "", (articles.category = "${req.params.category}"), 1=1))
-//     ORDER BY (sortCol) DESC
-//     LIMIT 10;`);
-// 	con.getConnection(res, (response) => {
-// 	  if (response.message == 'fail') return;
-// 	  response.conn.query(`SELECT articles.*,
-//     CASE WHEN "${req.params.filter}" = "date" THEN articles.datePosted
-//       WHEN "${req.params.filter}" = "rating" THEN r.rank
-//       ELSE articles.title
-//       END AS sortCol
-//       FROM articles
-//       LEFT OUTER JOIN profiles ON articles.author = profiles.ID
-//       LEFT OUTER JOIN (SELECT AVG(ranking) AS rank, author FROM reviews GROUP BY author) AS r ON articles.author = r.author
-//       WHERE
-//         ((profiles.name LIKE '%${req.params.input}%' AND "${req.params.filter}" != "article") OR (articles.title LIKE '%${req.params.input}%' AND "${req.params.filter}" != "author"))
-//           AND
-//         (IF("${req.params.category}" != "", (articles.category = "${req.params.category}"), 1=1))
-//       ORDER BY (sortCol) DESC
-//       LIMIT 10;`, function(err, results, fields) {
-// 		res.send(results);
-// 	  });
-// 	});
-// });
 
 router.get('/api/articles', async (req, res) => {
   con.getConnection(res, (response) => {
@@ -124,7 +82,6 @@ router.get('/api/articles', async (req, res) => {
     });
   });
 });
-
 
 
 router.put('/api/articles/:articleId', async (req, res) => {
@@ -147,63 +104,6 @@ router.post('/api/articles', async (req, res) => {
     });
   });
 });
-
-//WHERE category = ${req.body.category}
-
-// router.get('/api/home', async (req, res) => {
-// 	con.getConnection(res, (response) => {
-// 		if (response.message == 'fail') return;
-// 		response.conn.query("SELECT * FROM articles ORDER BY datePosted DESC LIMIT 10",
-// 		  function (err, result, fields) {
-// 			res.send(JSON.stringify(result));
-// 		});
-// 	});
-// });
-
-
-// router.get('/api/home/userID', async (req, res) => {
-// 	con.getConnection(res, (response) => {
-// 		if (response.message == 'fail') return;
-// 		response.conn.query(`SELECT * FROM articles where author = ${req.body.author}
-// 		 ORDER BY datePosted DESC LIMIT 10`, function (err, result, fields) {
-// 			res.send(JSON.stringify(result));
-// 		});
-// 	});
-// });
-
-
-// router.get('/api/browse/category', async (req, res) => {
-// 	con.getConnection(res, (response) => {
-// 		if (response.message == 'fail') return;
-// 		response.conn.query(`SELECT * FROM articles where category = "${req.body.category}"`,
-// 		 function (err, result, fields) {
-// 			res.send(JSON.stringify(result));
-// 		});
-// 	});
-// });
-
-
-// router.get('/api/articles/:articleID', async (req, res) => {
-//   con.getConnection(res, (response) => {
-//     if (response.message == 'fail') return;
-//     response.conn.query("SELECT * FROM articles", function(err, result, fields) {
-//       res.send(JSON.stringify(result));
-//     });
-//   });
-// });
-
-
-// router.post('/api/articles', async (req, res) => {
-//   con.getConnection(res, (response) => {
-//     if (response.message == 'fail') return;
-//     response.conn.query(`INSERT INTO articles (title,content,author,price,image,category) VALUES
-//                 ("${req.body.title}","${req.body.cont}",${req.body.author},${req.body.price},
-//                 "${req.body.image}","${req.body.category}")`, function(err, result, fields) {
-//       res.send(JSON.stringify(result));
-//     });
-//   });
-// });
-
 
 router.delete('/api/articles/:articleId', async (req, res) => {
   con.getConnection(res, (response) => {
